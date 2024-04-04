680982d6f05a758d75a9fcf5685e8f51c5597c5e
C3LZPC <c3lzpc@inf.elte.hu>
2024-04-04 20:38:14 +0200

    Bugfix: map scale

--- main.gd
+++ main.gd
@@ -35,17 +35,21 @@ func _ready():
 	var map_metadata = JSON.parse_string(FileAccess.get_file_as_string("res://data/" + str(map_step) + "/metadata.json"))
 	if map_metadata:
 		height_offset = map_metadata["min_height"]
-		var map_max_x = 10
-		var map_max_y = 10
+		var map_max_x = 5
+		var map_max_y = 5
 		#var map_max_x = int(map_metadata["x"])
 		#var map_max_y = int(map_metadata["y"])
+		
+		get_node("Camera3D").translate(Vector3(map_max_y * 0.5 * 0.5 * map_step, 0, map_max_x * 0.5 * 0.5 * map_step))
+		
 		for x in map_max_x:
 			threads[x] = {}
 			for y in map_max_y:
 				var terrain = HTerrain.new()
 				get_node("TerrainBase").add_child(terrain)
-				terrain.translate(Vector3(y * map_step, height_offset, x * map_step))
+				terrain.translate(Vector3(y * 0.5 * map_step, height_offset, x * 0.5 * map_step))
 				terrain.name = str(x) + "_" + str(y)
+				terrain.scale = Vector3(0.5, 1, 0.5)
 				#terrain.centered = true
 				terrain.set_shader_type(HTerrain.SHADER_LOW_POLY)
 				
